f5684ea9403c4d121b05466d552671458ebd7428
Paul Schlattmann <3031146+pschlatt@users.noreply.github.com>
2019-03-28 15:59:48 -0600

    Starts Authorization

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password 
   # e.g., User.authenticate('penelope@turing.com', 'boom')
   def self.authenticate(email, password)
     # if email and password correspond to a valid user, return that user
